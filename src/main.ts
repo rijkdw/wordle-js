817e23850d096bf42ff088d13f21b853fa97cee9
Rijk de Wet <rijkdw@gmail.com>
2023-10-02 21:48:18 +0200

    fix trailing slash issue

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,14 @@ import { View } from "./view";
 
 function getWord() {
   const searchParams = new URLSearchParams(window.location.search);
-  let word = searchParams.get("word");
-  if (word === null) {
+  let word = searchParams.get("word")?.substring(0, 5);
+  if (word === null || word === undefined) {
     word = "HELLO";
   }
   return word.toUpperCase();
 }
+
+console.log(getWord());
 
 const model = new Model(getWord());
 const view = new View();
